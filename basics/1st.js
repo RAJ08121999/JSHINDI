138c4a9adb9e13998c800d1022a5dd5760003ed1
MD RAJ <rajmd1800@gmail.com>
2024-08-19 13:38:22 +0530

    second code

---
+++ basics/1st.js
@@ -0,0 +1,9 @@
+console.log("hey its a java script code");
+
+const product ={
+    title:"ball pen",
+    rate:100,
+    isDeal: true,
+    rating: 4
+};
+console.log(product)
---
+++ operators/operatorspractice.js
@@ -0,0 +1,23 @@
+// it is a single line comment
+/* it is a multi line comment 
+i am going to comment these lines*/
+
+// Arithmetic operators
+
+let num1=5;
+let num2=7;
+console.log("a=",num1,"+"+"b=",num2,"=",num1+num2);
+console.log("a=",num1,"+"+"b=",num2,"=",num1-num2);
+console.log("a=",num1,"+"+"b=",num2,"=",num1*num2);
+console.log("a=",num1,"+"+"b=",num2,"=",num1/num2);
+console.log("a=",num1,"+"+"b=",num2,"=",num1%num2);
+
+console.log("a=",num1);
+console.log("b=",num2);
+//unary operator
+
+num1=++num1;
+console.log(num1)
+// console.log(num1);
+num1=--num1
+console.log(num1)
