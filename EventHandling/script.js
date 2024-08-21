415c4ebff728f898c352cca75b12d3182cdc0dd4
MD RAJ <rajmd1800@gmail.com>
2024-08-21 11:32:00 +0530

    partially completed

---
+++ EventHandling/script.js
@@ -0,0 +1,10 @@
+let btn1=document.querySelector("#btn1");
+btn1.onclick = ()=>{
+    console.log("button clicked");
+}
+
+let division=document.querySelector("#div1");
+division.onmouseover =()=>
+{
+    console.log("you are inside the div");
+}
